a8f4a112e053342bc0e8af73b8a139c672451df5
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2021-06-12 18:10:17 -0400

    Create Remove Duplicates From an Unsorted Linked List.java

---
+++ Remove Duplicates From an Unsorted Linked List/Remove Duplicates From an Unsorted Linked List.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicatesUnsorted(ListNode head) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        ListNode curr = head;
+        while(curr!=null){
+            map.put(curr.val,map.getOrDefault(curr.val,0)+1);
+            curr = curr.next;
+        }
+        ListNode newHead = new ListNode(0);
+        curr = newHead;
+        while(head!=null){
+            if(map.get(head.val) == 1){
+                curr.next = head;
+                curr = curr.next;   
+            } 
+            head = head.next;
+        }
+        curr.next = null;
+       
+        return newHead.next;
+    }
+}
