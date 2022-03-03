d0f60bbbcaca9f57a1e45e8feadc9e6aee2033aa
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-03-02 22:31:52 -0500

    Time: 9 ms (47.91%), Space: 42.4 MB (80.80%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> counts = new HashMap<>();
+        for(char c : magazine.toCharArray()){
+            counts.put(c,counts.getOrDefault(c,0)+1);
+        }
+        for(char c : ransomNote.toCharArray()){
+            if(!counts.containsKey(c)){
+                return false;
+            }
+            else{
+                if(counts.get(c) == 0){
+                    return false;
+                }
+            }
+            counts.put(c,counts.get(c)-1);
+        }
+        
+        return true;
+    }
+}
