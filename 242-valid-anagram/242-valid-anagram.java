53ba78f8ee58ae109bbd3bc2120f9846406ed902
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-06-15 20:14:06 -0400

    Time: 2 ms (98.65%), Space: 42.3 MB (92.09%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
+       int[] s_count = new int[26];
+       int[] t_count = new int[26];
         
-        //Two unequal length strings cannot be anagrams since some characterrs 
-        //will be missing 
+       if(s.length() != t.length()){
+           return false;
+       } 
         
-        if(s.length() != t.length()) return false;
+       for(char c : s.toCharArray()){
+           s_count[c-'a'] = s_count[c-'a'] + 1;  
+       }
+       
+       for(char c : t.toCharArray()){
+           t_count[c-'a'] = t_count[c-'a'] + 1;  
+       }
         
-        for(int i = 0;i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
-        }
-        
-        for(var entry : map.entrySet()){
-            if(entry.getValue()!=0){
-                return false;
-            }
-        }
+       for(int i=0;i<s_count.length;i++){
+           if(s_count[i] != t_count[i]){
+               return false;
+           }
+       } 
         
         return true;
+        
     }
 }
