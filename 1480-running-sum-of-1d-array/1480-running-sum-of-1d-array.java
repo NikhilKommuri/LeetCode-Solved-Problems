bfdbe0925901813863857b4deea4ff491a7bba07
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-06-20 18:48:25 -0400

    Time: 0 ms (100.00%), Space: 42 MB (94.12%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        if(nums==null || nums.length<0){
+            return null;
+        }
+        for(int i=1;i<nums.length;i++){
+          nums[i] = nums[i] + nums[i-1];
+        }
+        
+        return nums;
+    }
+}
