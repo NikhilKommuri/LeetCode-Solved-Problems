4193f4f087f148b3bd9c8447da9258dc20f74c83
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-06-25 18:21:48 -0400

    Time: 5 ms (94.83%), Space: 43 MB (63.79%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,24 +1,22 @@
 class Solution {
      public int characterReplacement(String s, int k) {
         int[] freq = new int[26];
-        int mostFreqLetter = 0;
-        int left = 0;
-        int max = 0;
-        
-        for(int right = 0; right < s.length(); right++){
-            freq[s.charAt(right) - 'A']++;
-            mostFreqLetter = Math.max(mostFreqLetter, freq[s.charAt(right) - 'A']);
-            
-            int lettersToChange = (right - left + 1) - mostFreqLetter;
-            while(lettersToChange > k){
-                freq[s.charAt(left) - 'A']--;
-                left++;
-                lettersToChange = (right - left + 1) - mostFreqLetter;
+        int N = s.length();
+        int windowStart = 0;
+        int maxcharCount = 0;
+        int maxLength = 0; 
+        for(int windowEnd =0; windowEnd<N;windowEnd++){
+            freq[s.charAt(windowEnd) - 'A']++;
+            int currCharCount = freq[s.charAt(windowEnd) - 'A'];
+            maxcharCount = Math.max(maxcharCount,currCharCount);
+            while(windowEnd - windowStart - maxcharCount + 1 > k){
+                freq[s.charAt(windowStart) - 'A']--;
+                windowStart++;
             }
             
-            max = Math.max(max, right - left + 1);
-        }
-        
-        return max;
+            maxLength = Math.max(maxLength,windowEnd - windowStart + 1);
+            
+        } 
+        return maxLength;
     }
 }
