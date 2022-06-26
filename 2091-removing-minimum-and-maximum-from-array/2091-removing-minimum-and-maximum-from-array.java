89c1bb93a8ce110d17f87567f52d2bd3b4a767b1
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-06-26 12:31:51 -0400

    Time: 3 ms (79.36%), Space: 55.9 MB (84.57%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        int max = 0,min = 0;
+        int maxIdx = 0,minIdx = 0;
         for(int i = 1; i < nums.length; i++) // We would find the index of Max & Min Elements here
         {
-            max = nums[i] > nums[max] ? i : max;
-            min = nums[i] < nums[min] ? i : min;
+            maxIdx = nums[i] > nums[maxIdx] ? i : maxIdx;
+            minIdx = nums[i] < nums[minIdx] ? i : minIdx;
         }
-        int front = Math.max(max,min) + 1; // To check the case when we remove elements from the Front
-        int back = nums.length - Math.min(max,min); // To check the case when we remove elements from the Back
-        int both = Math.min(max,min) + 1 + nums.length - Math.max(min,max); // To check the case when we remove elements from both ends
+        int front = Math.max(maxIdx,minIdx) + 1; // To check the case when we remove elements from the Front
+        int back = nums.length - Math.min(maxIdx,minIdx); // To check the case when we remove elements from the Back
+        int both = Math.min(maxIdx,minIdx) + 1 + nums.length - Math.max(minIdx,maxIdx); // To check the case when we remove elements from both ends
         return Math.min(both, Math.min(front, back)); // return the minimum of all
     }
 }
