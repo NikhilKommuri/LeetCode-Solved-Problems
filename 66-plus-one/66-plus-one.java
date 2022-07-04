40277847a668f2f312de9ae0377a7e4ab7b27123
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-07-04 15:49:39 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (61.61%) - LeetHub

---
+++ 66-plus-one/66-plus-one.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits == null) {
+            return null;
+        }
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            
+            }
+        }
+
+        // Code reaches here for inputs of all 9s such as [9, 9, 9]
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+ }
+}    
