faa852234173c76367591e3342c8bfffb86f06f7
Nikhil Kommuri <44361996+NikhilKommuri@users.noreply.github.com>
2022-07-04 12:14:15 -0400

    Time: 3 ms (22.63%), Space: 41.8 MB (70.14%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.java
+++ 362-design-hit-counter/362-design-hit-counter.java
@@ -1,21 +1,48 @@
 class HitCounter {
-    Queue<Integer> queue;
+
+    private int total;
+    private Deque<Pair<Integer, Integer>> hits; 
+
+    /** Initialize your data structure here. */
     public HitCounter() {
-        queue = new LinkedList<>();
+        // Initialize total to 0
+        this.total = 0;
+        this.hits = new LinkedList<Pair<Integer, Integer>>();
     }
     
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
-        queue.add(timestamp);
-              while(!queue.isEmpty() && (timestamp - queue.peek()) >= 300){
-             queue.poll();
-            }
+        if (this.hits.isEmpty() || this.hits.getLast().getKey() != timestamp) {
+            // Insert the new timestamp with count = 1
+            this.hits.add(new Pair<Integer, Integer>(timestamp, 1));
+        } else {
+            // Update the count of latest timestamp by incrementing the count by 1
+
+            // Obtain the current count of the latest timestamp 
+            int prevCount = this.hits.getLast().getValue();
+            // Remove the last pair of (timestamp, count) from the deque
+            this.hits.removeLast();
+            // Insert a new pair of (timestamp, updated count) in the deque
+            this.hits.add(new Pair<Integer, Integer>(timestamp, prevCount + 1));
         }
+        // Increment total
+        this.total++;
+    }
     
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-      while(!queue.isEmpty() && (timestamp - queue.peek()) >= 300){
-             queue.poll();
+        while (!this.hits.isEmpty()) {
+            int diff = timestamp - this.hits.getFirst().getKey();
+            if (diff >= 300) {
+                // Decrement total by the count of the oldest timestamp
+                this.total -= this.hits.getFirst().getValue();
+                this.hits.removeFirst();
             }
-        return queue.size();
+            else break;
+        }
+        return this.total;
     }
 }
 
